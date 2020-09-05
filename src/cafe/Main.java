7b1a48b97ad6aa2daaeb43f10dbe80ff12665175
Kevin Mera <kamcak@hotmail.es>
2020-09-04 19:27:06 -0500

    Second

--- src/cafe/Main.java
+++ src/cafe/Main.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -24,7 +27,8 @@ public class Main {
 		for(int i=1; i<=n; i++) { 
 
 			//Read Line
-			String line = br.readLine();
+			String line = "";
+			line = br.readLine();
 			String[] yearsLine = line.split(" ");
 
 			double[] values = new double[yearsLine.length];	
@@ -35,23 +39,43 @@ public class Main {
 			}
 
 			//Sorting 
+			int count = 0;
+			int[] timing = new int[values.length-1];
 			for (int k = values.length-1; k > 0; k--) {
 				for (int j = 0; j < k; j++) {
 					if(values[j] > values[j+1]) {
 						double temp = values[j];
 						values[j] = values[j+1];
 						values[j+1] = temp;
+						count++;
 					}
 				}
+				timing[k-1] = count;
+				count = 0;
 			}
+			double sum1 = 0;
+			for (int j = 0; j < timing.length; j++) {
 
+				sum1 += timing[j];
+
+			}
+			double total = 0;
+			total = sum1 / (double)timing.length;
+			
+			BigDecimal formatNumber = new BigDecimal(total);
+			formatNumber = formatNumber.setScale(2, RoundingMode.DOWN);
+			
+			formatNumber.shortValueExact();
+			
 			//output
 			String output = "";
-			for (int l = 0; l < values.length; l++) {
+			output += formatNumber+"-";
+			for (int l = 0; l < values.length-1; l++) {
 				output += values[l] + " "; 
 			}
-			bw.write(output);
-			bw.flush();
+			output += values[values.length-1];
+			bw.write(output+"\n");
 		}
+		bw.flush();
 	}
 }
